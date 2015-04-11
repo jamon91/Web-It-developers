6ba4545aac6345563553b3a79c88dcdf1580f2fe
edcyrus <edcyrus@gmail.com>
2015-04-11 15:13:50 -0600

    Metodos Connector

--- CodigoProyecto/lib/ConnectordeDatos.php
+++ CodigoProyecto/lib/ConnectordeDatos.php
@@ -15,23 +15,45 @@ class ConectordeBD
         try {
             //armar String de conexion
             //modificar el string de conexion para que funcione con su base de datos
-            self:: $dbh = new PDO("mysql:host=$host;dbname=$db", self::$user, self::$password);
+            self:: $dbh = new PDO("mysql:host=self::$host;dbname= self::$db", self::$user, self::$password);
             echo "Success";
 
 
         } catch (PDOException $e) {
-            /*echo $e->getTraceAsString();
-            print "Error!: " .*/
-            echo $e->getMessage(); //. "<br/>";
+            echo $e->getTraceAsString();
+            print "Error!: " .
+            echo $e->getMessage(); . "<br/>";
             die();
         }
     }
 
+    public static function registrarUsuario($id,$idAdmin,$nombre,$apellido,$email,$password,$estado,$tipoUsuario){
+            self::abrirConexion();
+             $sql = "INSERT INTO Usuario (idUsuario,idAdmin,Nombre,Apellidos,Email,Password,Estado,TipoUsuario)
+                     VALUES ('$id','$idAdmin','$nombre','$apellido','$email','$password','$estado','$estado','$tipoUsuario')";
+
+            self::ejecutarQuery($sql);
+    }
+
+    public  static function buscarUsuario($id){
+        $sql= 'SELECT nombre , apellido FROM Usuario WHERE idUsuario="$id"';
+        self::ejecutarQuery($sql);
+    }
+
+    public static function modificarUsuario($id,$estado,$tipoUsuario){
+        $sql =   "UPDATE Usuario.
+                  SET Estado = '$estado' , TipoUsuario = '$tipoUsuario'.
+                  WHERE idUsuario= $id";
+
+
+            self::ejecutarQuery($sql);
+    }
 
     public static function ejecutarQuery($sql)
     {
 
         $dbh->query($sql);
+        return echo "Solicitud ejecutada exitosmente";
 
     }
 
@@ -42,7 +64,7 @@ class ConectordeBD
         foreach ($dbh->query($sql) as $row) {
             print $row['ID'] . "\t";
             print $row['nombre'] . "\n";
-            //print $row['calories'] . "\n";
+
         }
     }
 }
